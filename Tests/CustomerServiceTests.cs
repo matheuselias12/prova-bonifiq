37859c6e17b6700d92abf3f59507579d0a58854d
Matheus Henrique <matheus_henryque007@hotmail.com>
2023-04-13 11:16:53 -0300

    teste unitario

--- Tests/CustomerServiceTests.cs
+++ Tests/CustomerServiceTests.cs
@@ -1,6 +1,29 @@
-﻿namespace ProvaPub.Tests
+﻿using ProvaPub.Services;
+using Xunit;
+
+namespace ProvaPub.Tests
 {
     public class CustomerServiceTests
     {
+        CustomerService _customerService;
+        public CustomerServiceTests(CustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [Fact]
+        public async void Teste()
+        {
+
+            // Arrange
+            decimal purchaseValue = 1000;
+            int customerId = 1;
+
+            // Act
+            var result = await _customerService.CanPurchase(customerId,purchaseValue);
+
+            // Assert
+            Assert.True(result);
+        }
     }
 }
